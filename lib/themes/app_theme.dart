d228969e93be9ef5b0be4c343bffd5d3e7fc50bd
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-03 17:05:40 -0300

    add new color

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -5,10 +5,19 @@ class AppTheme {
     return ThemeData(
       fontFamily: 'Montserrat',
       colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFFFBF5CA),
-          secondary: const Color(0xFFF5D70A),
-          tertiary: Colors.black),
+        primary: const Color(0xFFFBF5CA),
+        secondary: const Color(0xFFF5D70A),
+        tertiary: Colors.black,
+        secondaryContainer: const Color(0xFFF2ECCB),
+      ),
       scaffoldBackgroundColor: Colors.white,
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
