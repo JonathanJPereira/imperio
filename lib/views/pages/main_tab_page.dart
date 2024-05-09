0dfcf888c5660d2aea2f636ddff62f550d22c37f
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-09 15:18:02 -0300

    Add button and profile picture in navbar

--- lib/views/pages/main_tab_page.dart
+++ lib/views/pages/main_tab_page.dart
@@ -27,23 +27,63 @@ class MainTabPage extends StatelessWidget {
         return SafeArea(
           child: Padding(
             padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 8.0),
-            child: RoundedNavBar(
-              destinations: [
-                ...List.generate(store.navItems.length, (index) {
-                  return RoundedDestination(
-                    icon: store.navItems[index].icon,
-                    selectedIcon: store.navItems[index].selectedIcon,
-                    selected: store.selectedIndex == index,
-                  );
-                })
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _buildMenuButton(context),
+                _buildNavBar(),
+                _buildProfilePicture()
               ],
-              onItemSelected: (index) {
-                store.setIndex(index);
-              },
             ),
           ),
         );
       }),
+    );
+  }
+
+  Widget _buildMenuButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(14),
+          backgroundColor: Theme.of(context).colorScheme.tertiary,
+          foregroundColor: Colors.white.withOpacity(0.9)),
+      child: const Icon(Icons.menu),
+    );
+  }
+
+  Widget _buildNavBar() {
+    return RoundedNavBar(
+      destinations: [
+        ...List.generate(store.navItems.length, (index) {
+          return RoundedDestination(
+            icon: store.navItems[index].icon,
+            selectedIcon: store.navItems[index].selectedIcon,
+            selected: store.selectedIndex == index,
+          );
+        })
+      ],
+      onItemSelected: (index) {
+        store.setIndex(index);
+      },
+    );
+  }
+
+  Widget _buildProfilePicture() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.black,
+          width: 2,
+        ),
+      ),
+      child: const CircleAvatar(
+        radius: 20,
+        backgroundImage: AssetImage('assets/images/perfil.png'),
+      ),
     );
   }
 }
