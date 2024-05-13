8824030bd7d080e9b215909400d63fa956678598
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-13 15:25:45 -0300

    Finish football pitch

--- lib/views/pages/match_details_page.dart
+++ lib/views/pages/match_details_page.dart
@@ -16,27 +16,43 @@ class MatchDetails extends StatelessWidget {
         title: 'Partida',
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       ),
-      body: Column(
-        children: [
-          const Divider(),
-          MatchCard(match: match),
-          buildMatchImageStack(match),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Divider(),
+            MatchCard(match: match),
+            buildMatchImageStack(match, context),
+          ],
+        ),
       ),
     );
   }
 
-  Widget buildMatchImageStack(Match match) {
+  Widget buildMatchImageStack(Match match, BuildContext context) {
     return Stack(
       alignment: Alignment.center,
       children: [
+        Container(
+          height: 200.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Theme.of(context).colorScheme.primary,
+                Theme.of(context).scaffoldBackgroundColor
+              ],
+            ),
+          ),
+        ),
         Image.asset(
           'assets/images/football_pitch.png',
           width: double.infinity,
           fit: BoxFit.cover,
         ),
-        positionTeamImage(match.teamAImage, const Alignment(-0.78, -0.9)),
-        positionTeamImage(match.teamBImage, const Alignment(0.78, -0.9)),
+        positionTeamImage(match.teamAImage, const Alignment(-0.78, -0.5)),
+        positionTeamImage(match.teamBImage, const Alignment(0.78, -0.5)),
       ],
     );
   }
@@ -46,10 +62,10 @@ class MatchDetails extends StatelessWidget {
       child: Align(
         alignment: alignment,
         child: Container(
-            padding: EdgeInsets.all(8),
+            padding: const EdgeInsets.all(8),
             decoration: BoxDecoration(
               border: Border.all(color: Colors.white, width: 1.25),
-              color: Color(0xFFDEE0DF),
+              color: const Color(0xFFDEE0DF),
               borderRadius: BorderRadius.circular(62.5),
             ),
             child: CustomNetworkImage(
