c3a8d5ac5daa10f17ae07dade0a11d330d8d8508
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-16 19:42:39 -0300

    change toast to show in top

--- lib/utils/toast_notifier.dart
+++ lib/utils/toast_notifier.dart
@@ -6,7 +6,7 @@ class ToastNotifier {
     Fluttertoast.showToast(
       msg: message,
       toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.BOTTOM,
+      gravity: ToastGravity.TOP,
       timeInSecForIosWeb: 4,
       backgroundColor: Colors.red,
       textColor: Colors.white,
