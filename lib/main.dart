4e8aeddd0ea2dd8085b83b7dee00fb9d75397cc3
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-20 07:32:07 -0300

    Uncommenting login logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
 import 'package:imperio/config/app_navigation.dart';
 import 'package:imperio/config/setup_app.dart';
+import 'package:imperio/stores/login/login_store.dart';
 import 'package:imperio/themes/app_theme.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   await setupApp();
 
-  // final loginStore = GetIt.instance<LoginStore>();
+  final loginStore = GetIt.instance<LoginStore>();
 
-  // final bool hasToken = await loginStore.checkToken();
+  final bool hasToken = await loginStore.checkToken();
 
-  // if (hasToken) {
-  //   await loginStore.refreshToken();
-  // }
+  if (hasToken) {
+    // await loginStore.refreshToken();
+  }
 
-  runApp(MyApp(hasToken: true));
+  runApp(MyApp(hasToken: hasToken));
 }
 
 class MyApp extends StatelessWidget {
