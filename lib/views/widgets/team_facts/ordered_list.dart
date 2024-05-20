b1f3a4ec4720f43883ea5ef1af11338bf0106b21
jonathanjpereira <jonathan.jpere@gmail.com>
2024-05-20 08:13:58 -0300

    Returning to list view, because on Android it was bugged

--- lib/views/widgets/team_facts/ordered_list.dart
+++ lib/views/widgets/team_facts/ordered_list.dart
@@ -10,35 +10,32 @@ class OrderedList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: List.generate(
-          items.length,
-          (index) => Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  '${index + 1}. ',
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+          child: Row(
+            children: <Widget>[
+              Text(
+                '${index + 1}. ',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  items[index],
                   style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
+                    fontSize: 12,
                   ),
                 ),
-                Expanded(
-                  child: Text(
-                    items[index],
-                    style: const TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
